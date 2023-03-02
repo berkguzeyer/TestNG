fb3a6d067c00d19e87a5074a5192f7334de5dc51
berkg <berkguzeyer@yahoo.com>
2023-03-01 19:03:45 -0500

    TestNG - Day3 and XML have been added.

---
+++ src/test/java/day03/_01_Dependency.java
@@ -0,0 +1,30 @@
+package day03;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _01_Dependency {
+
+    @Test
+    void startCar(){
+        System.out.println("Car has started.");
+//        Assert.fail();
+    }
+
+    @Test(dependsOnMethods = {"startCar"})
+        void driveCar(){
+            System.out.println("Car is driven.");
+    }
+
+    @Test(dependsOnMethods = {"startCar","driveCar"})
+    void parkCar(){
+        System.out.println("Car is parked.");
+    }
+
+    @Test(dependsOnMethods = {"parkCar", "driveCar", "startCar"})
+    void stopCar(){
+        System.out.println("Car has stopped.");
+    }
+
+
+}
